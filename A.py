f6e2b59fe932bdb0858fe3b4548b3c1f1f6450b6
shachar1236 <shachar5cohen@gmail.com>
2020-03-11 14:32:50 +0200

    Finish

--- A.py
+++ A.py
@@ -32,12 +32,11 @@ class Node():
         self.f = h + g
 
 de_board = [
-    ["#", "#", "#", "#", "#", "#"],
-    ["#", "S", " ", " ", " ", "#"],
-    ["#", " ", "#", " ", " ", "#"],
-    ["#", " ", " ", "#", " ", "#"],
-    ["#", " ", " ", " ", "E", "#"],
-    ["#", "#", "#", "#", "#", "#"],
+    ["S", " ", " ", "#", " ", "E"],
+    [" ", " ", " ", "#", " ", " "],
+    [" ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", "#", " ", " "],
+    [" ", " ", " ", " ", " ", " "],
 ]
 
 def getDistance(x1, x2, y1, y2):
@@ -68,26 +67,51 @@ def getNodes(board,x, y, s, e):
     nodes = []
     for move in (-1, 1):
         try:
-            if board[y][x + move] == " " and x + move > 0:
+            if board[y][x + move] == " " and x + move >= 0:
                 nodes.append(Node(x+move, y, getDistance(s.x, x+move, s.y, y), getDistance(e.x, x+move, e.y, y)))
         except:
             pass
         try:
-            if board[y + move][x] == " " and y + move > 0:
+            if board[y + move][x] == " " and y + move >= 0:
                 nodes.append(Node(x, y+move, getDistance(s.x, x, s.y, y+move), getDistance(e.x, x, e.y, y+move)))
         except:
             pass
         try:
-            if board[y+move][x+move] == " " and y + move > 0 and x + move > 0:
+            if board[y+move][x+move] == " " and y + move >= 0 and x + move >= 0:
                 nodes.append(Node(x+move, y+move, getDistance(s.x, x+move, s.y, y+move), getDistance(e.x, x+move, e.y, y+move)))
         except:
             pass
         try:
-            if board[y-move][x+move] == " " and y-move > 0 and x+move > 0:
-                nodes.append(Node(x-move, y+move, getDistance(s.x, x+move, s.y, y-move), getDistance(e.x, x+move, e.y, y-move)))
+            if board[y-move][x+move] == " " and y-move >= 0 and x+move >= 0:
+                nodes.append(Node(x+move, y-move, getDistance(s.x, x+move, s.y, y-move), getDistance(e.x, x+move, e.y, y-move)))
         except:
             pass
     return nodes
+
+def checkEnd(board, end):
+    for move in (-1, 1):
+        try:
+            if board[end.y][end.x + move] == "+" and end.x + move >= 0:
+                return True
+        except:
+            pass
+        try:
+            if board[end.y + move][end.x] == "+" and end.y + move >= 0:
+                return True
+        except:
+            pass
+        try:
+            if board[end.y+move][end.x+move] == "+" and end.y + move >= 0 and end.x + move >= 0:
+                return True
+        except:
+            pass
+        try:
+            if board[end.y-move][end.x+move] == "+" and end.y-move >= 0 and end.x+move >= 0:
+                return True
+        except:
+            pass
+    return False
+    
 
 board = deepcopy(de_board)
 start, end = getStartEnd(board)
@@ -97,14 +121,16 @@ printBoard(board)
 
 lastNode = None
 run = True
-while run:
+while not checkEnd(board, end):
     m = Node(0,0,float("inf"), float("inf"))
     fs = []
     m_i = 0
+    changed = False
     for node_index ,node in enumerate(nodes):
         if m.f > node.f and not node.visited:
             m = node
             m_i = node_index
+            changed = True
         fs.append(node.f)
     fs.pop(m_i)
     f = m.f
@@ -113,24 +139,28 @@ while run:
             if m.h > node.h and not node.visited and node.f == f:
                 m = node
                 m_i = node_index
-    nodes[m_i].visited = True
-    nodes[m_i].parent = lastNode
-    board[m.y][m.x] = "+"
-    nodes += getNodes(board, m.x, m.y, start, end)
+                changed = True
+    if not changed:
+        nodes += getNodes(board, start.x, start.y, start, end)
+        lastNode = None
+        board = deepcopy(de_board)
+    else:
+        nodes[m_i].visited = True
+        nodes[m_i].parent = lastNode
+        board[m.y][m.x] = "+"
+        nodes += getNodes(board, m.x, m.y, start, end)
+        lastNode = m
     print()
     printBoard(board)
     nodes = list(set(nodes))
-    lastNode = m
+    
 
-
-run = True
-for node in nodes:
-    print()
-    printBoard(board)
+board = deepcopy(de_board)
+node = lastNode
+while not checkEnd(board, start):
     if node.visited:
         board[node.y][node.x] = "+"
+    node = node.parent
 
-
-print()
 print()
 printBoard(board)
